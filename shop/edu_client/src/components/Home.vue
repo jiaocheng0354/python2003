695d1d1a865b5f4c0b64fa2675b9a5bd5ec826b9
jiaocheng0354 <357883918@qq.com>
2020-07-09 08:35:46 +0800

    新建项目

---
+++ shop/edu_client/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+         <Header></Header>
+        <Banner></Banner>
+        <Fotter></Fotter>
+    </div>
+</template>
+
+<script>
+    import Header from "./Header";
+    import Banner from "./Banner";
+    import Fotter from "./Fotter";
+
+    export default {
+        name: "Home",
+        components: {
+            "Header": Header,
+            "Banner": Banner,
+            "Fotter": Fotter,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
